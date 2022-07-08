e165d93d3592ea3bb338d94ef14db15aa83e8bf5
Juwon Oh <13a71032776@gmail.com>
2022-07-08 20:41:50 +0900

    study: inflean 5-5

---
+++ inflearn/mysolution/5-5.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+
+sys.stdin = open("inflearn/mysolution/input.txt", "rt")
+
+n, k = map(int, input().split())
+p_list = deque(_ for _ in range(1, n+1))
+
+while p_list:
+    for _ in range(k-1):
+        cur = p_list.popleft()
+        print(cur)
+        p_list.append(cur)
+    p_list.popleft()
+    if len(p_list) == 1:
+        print(p_list[0])
