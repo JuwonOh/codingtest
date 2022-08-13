0f7f6917f0938dae34381c7bf35093e7323bfafa
Juwon Oh <13a71032776@gmail.com>
2022-08-13 14:12:58 +0900

    study: programmers njinsu game

---
+++ programmers/n진수게임-구현,dfs.py
@@ -0,0 +1,24 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/17687
+
+n, t, m, p = 2, 4, 2, 1
+
+
+def convert(num, geesu):
+    nums = "01234567890ABCDEF"
+    q, r = divmod(num, geesu)
+    if q == 0:
+        return nums[r]
+    else:
+        print(q, geesu)
+        return convert(q, geesu) + nums[r]
+
+def solution(n, t, m, p):
+    answer = ""
+    temp = ""
+    for i in range(t * m):
+        temp += convert(i, n)
+    for j in range(t):
+        answer += temp[p - 1 + m*j]
+    return answer
+
+solution(n, t, m, p)
