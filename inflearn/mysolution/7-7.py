b758e6a937c5b2a85e377d20a4a418cc2657f680
Juwon Oh <13a71032776@gmail.com>
2022-09-01 22:00:04 +0900

    study: inflearn 7 bfs

--- inflearn/mysolution/7-7.py
+++ inflearn/mysolution/7-7.py
@@ -4,25 +4,22 @@ from collections import deque
 sys.stdin = open("inflearn/mysolution/input.txt", "rt")
 
 max_t = 10000
-ch = [0] * (max_t+1)
-# 거리와 횟수를 나타내는 list
-dis = [0] * (max_t + 1)
+check = [0] * (max_t + 1)
+distance = [0] * (max_t + 1)
 n, m = map(int, input().split())
-# n이 출발지점
-ch[n] = 1
-dis[n] = 0
+check[n] = 1
+distance[n] = 0
 dQ = deque()
 dQ.append(n)
+
 while dQ:
     now = dQ.popleft()
     if now == m:
         break
-    # for문이 tuple값을 하나씩 탐색한다.
-    for next in (now-1, now + 1, now + 5):
-        if 0 < next <= max_t:
-            if ch[next] == 0:
-                dQ.append(next)
-                ch[next] = 1
-                dis[next] = dis[now] + 1
-
-print(dis[m])
+    for next_step in (now - 1, now + 1, now + 5):
+        if 0 < next_step < max_t:
+            if check[next_step] == 0:  # 방문을 안했을 때만
+                dQ.append(next_step)
+                check[next_step] = 1
+                distance[next_step] = distance[now] + 1
+print(distance[m])
